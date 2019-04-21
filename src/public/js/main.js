aaea87025d75c46035f66852efdf610bc25bd64b
Hassnian <hassnianxd@gmail.com>
2019-04-21 12:39:52 +0200

    basci setup

---
+++ src/public/js/main.js
@@ -0,0 +1,5 @@
+const React =require('react');
+const ReactDOM =require('react-dom');
+const App=()=><h1>Hello world</h1>
+ReactDOM.render(<App/>,document.getElementById('root'))
+
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path=require('path');
+const HtmlWebpackPlugin=require('html-webpack-plugin')
+module.exports={
+    entry:path.join(__dirname,"src","public","js","main.js"),
+    output:{
+        path:path.join(__dirname,"dist"),
+        filename:"boundle.js"
+    },
+    mode:"development",
+    module:{
+        rules:[
+            {
+                test:/\.jsx?/,
+                exclude:/node_modules/,
+                loader:"babel-loader",
+                query:{
+                    presets:['@babel/preset-env', '@babel/preset-react']
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+          template: path.join(__dirname,"src","views","index.html")
+        })
+      ]
+};
